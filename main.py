3d867fb170258a368e2db7f4eb446507f35b6a67
Qqww4599 <s93011@gmail.com>
2025-05-28 19:51:05 +0800

    測試新增mkfile功能

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def main():
+    mkfile()
     print("This is the main function of the application.")
 
 def test1():
@@ -6,6 +7,11 @@ def test1():
 
 def test2():
     print("This is test function 2.")
+
+def mkfile():
+    import os
+    with open("testfile.txt", "w") as f:
+        f.write("This is a test file.")
 
 if __name__ == "__main__":
     main()
